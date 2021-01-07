b2a0f18b01076f35cf4a0fc746898a4c96118186
aci686 <aaron.castro.sanchez@outlook.com>
2021-01-07 12:52:43 +0100

    Added bottom bar

--- .bin/launch_polybar.sh
+++ .bin/launch_polybar.sh
@@ -8,4 +8,6 @@ pgrep polybar | xargs kill -9
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launc bar
-polybar main -c ~/.config/polybar/config.ini &
+#polybar main -c ~/.config/polybar/config.ini &
+polybar -q top -c ~/.config/polybar/config.ini &
+polybar -q bottom -c ~/.config/polybar/config.ini &
