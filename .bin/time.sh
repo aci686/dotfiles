c6e77408f1fb5451df9aa35891b9bf825d66e891
aci686 <aaron.castro.sanchez@outlook.com>
2021-01-08 15:08:45 +0100

    Added datetime applet

---
+++ .bin/time.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+dir="$HOME/.config/rofi/applets/"
+rofi_command="rofi -theme $dir/time.rasi"
+
+## Get time and date
+TIME="$(date +"%I:%M %p")"
+DN=$(date +"%A")
+MN=$(date +"%B")
+DAY="$(date +"%d")"
+MONTH="$(date +"%m")"
+YEAR="$(date +"%Y")"
+
+options="$DAY\n$MONTH\n$YEAR"
+
+## Main
+chosen="$(echo -e "$options" | $rofi_command -p " $DN, $TIME" -dmenu)"
