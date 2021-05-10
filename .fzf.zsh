785c33536a18de11449afd016f447415bd663d99
Aaron <56502347+aci686@users.noreply.github.com>
2021-05-10 21:54:43 +0200

    Update .fzf.zsh

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
 if [[ ! "$PATH" == */opt/fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/home/[username]/.fzf/bin"
+  export PATH="${PATH:+${PATH}:}/home/whoami/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/[username]/.fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "/home/whoami/.fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/[username]/.fzf/shell/key-bindings.zsh"
+source "/home/whoami/.fzf/shell/key-bindings.zsh"
