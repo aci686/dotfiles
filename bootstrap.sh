993b24daefc4b4f55af448b5ea4132b913d42c4f
Aaron <56502347+aci686@users.noreply.github.com>
2021-05-10 21:39:15 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -146,7 +146,7 @@ lsd_check() {
     echo -e "[$info] Installing LSD (enhanced ls)..."
     cd $(mktemp -d)
     v=$(curl -s https://github.com/Peltoche/lsd/releases/latest | sed -E 's/.*"([^"]+)".*/\1/' | awk -F'/tag/' '{print $2}')
-    wget $(curl -s https://github.com/Peltoche/lsd/releases/latest/ | sed -E 's/.*"([^"]+)".*/\1/' | sed 's/tag/download/g')/lsd_$v_amd64.deb
+    wget $(curl -s https://github.com/Peltoche/lsd/releases/latest/ | sed -E 's/.*"([^"]+)".*/\1/' | sed 's/tag/download/g')/lsd_$(curl -s https://github.com/Peltoche/lsd/releases/latest | sed -E 's/.*"([^"]+)".*/\1/' | awk -F'/tag/' '{print $2}')_amd64.deb
     sudo dpkg -i *.deb
     if [ $? == 0 ]; then
         echo -e [$check]
